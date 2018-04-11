6e3ecbfdf827d9d61f233d7a562489f70cfa3682
argobuller <38219552+argobuller@users.noreply.github.com>
2018-04-11 14:49:05 +0200

    Create sample_log_v1.py

---
+++ Logging/Basic/sample_log_v1.py
@@ -0,0 +1,32 @@
+# File withot loging - print()
+
+def add(x, y):
+    """Add Function"""
+    return x + y
+
+def subtract(x, y):
+    """Substract Function"""
+    return x - y
+
+def multiply(x, y):
+    """Multiply Function"""
+    return x * y
+
+def divide(x, y):
+    """Divide Function"""
+    return x / y
+
+num_1 = 20
+num_2 = 10
+
+add_result = add(num_1, num_2)
+print('Add:{} + {} = {}'.format(num_1, num_2, add_result))
+
+sub_result = subtract(num_1, num_2)
+print('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
+
+mul_result = multiply(num_1, num_2)
+print('Mul: {} * {} = {}'.format(num_1, num_2, mul_result))
+
+div_result = divide(num_1, num_2)
+print('Div: {} / {} = {}'.format(num_1, num_2, div_result))
